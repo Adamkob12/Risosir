f873a4948a57335bb200cbfcea2777a5cf61bc2b
Adam <46227443+Adamkob12@users.noreply.github.com>
2024-07-06 23:32:37 +0300

    finished with start? (no way to test it yet)

--- src/kernel/src/kernelvec/mod.rs
+++ src/kernel/src/kernelvec/mod.rs
@@ -1,6 +1,28 @@
+use core::arch::asm;
+
 #[repr(align(4))]
 #[allow(unsafe_op_in_unsafe_fn)]
 #[no_mangle]
 pub unsafe fn timervec() {
-    todo!()
+    asm!("csrrw a0, mscratch, a0");
+    // a0 contains the address of [`DataToHandleTimerInt`], with a dedicated 3 places to save the value of registers.
+    asm!("sd a1, 0(a0)");
+    asm!("sd a2, 8(a0)");
+    asm!("sd a3, 16(a0)");
+
+    // 24(a0) will contain the address of the mtimecmp register
+    // 32(a0) will contain the desired amount of cycles to pass before the next timer interrupt
+    asm!("ld a1, 24(a0)");
+    asm!("ld a2, 32(a0)");
+    asm!("ld a3, 0(a1)");
+    asm!("add a2, a2, a3"); // Increment mtimecmp by the desired interval
+    asm!("sd a3, 0(a1)"); // Store the value back in the mtimecmp register
+
+    // Restore the registers
+    asm!("ld a1, 0(a0)");
+    asm!("ld a2, 8(a0)");
+    asm!("ld a3, 16(a0)");
+    asm!("csrrw a0, mscratch, a0");
+
+    asm!("mret");
 }
